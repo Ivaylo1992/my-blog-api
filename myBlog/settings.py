49494ec736a90d0fe26948d2c860877160dca272
Ivaylo1992 <ivailop123@gmail.com>
2025-02-28 06:39:50 +0200

    fix: fixed settings

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rdc4+_lr9rv+r17tm9jzbhtv=ng@27e8(s!31*5=*mqevggsmt"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if config("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = []
 
